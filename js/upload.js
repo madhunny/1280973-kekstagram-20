f2bb75650c0e9970a4a75cc7fecf30040941af39
Daria <madhunny@gmail.com>
2020-07-27 03:18:17 +0100

    correction errors in upload

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,13 @@
 'use strict';
 (function () {
 
+  function popupEscapePress(evt) {
+    if (evt.key === 'Escape' && evt.target !== window.constant.textHashtagsElement) {
+      evt.preventDefault();
+      closePopupImageModification();
+    }
+  }
+
   function openPopupImageModification() {
     var file = window.constant.uploadFileElement.files[0];
     window.constant.uploadImagePreviewElement.src = URL.createObjectURL(file);
@@ -18,19 +25,52 @@
     document.removeEventListener('keydown', popupEscapePress);
   }
 
-  window.constant.uploadFileElement.addEventListener('change', openPopupImageModification);
-  window.constant.uploadCancelButtonElement.addEventListener('click', closePopupImageModification);
-  window.constant.uploadCancelButtonElement.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter') {
-      closePopupImageModification();
-    }
-  });
-
-  function popupEscapePress(evt) {
-    if (evt.key === 'Escape' && evt.target !== window.constant.textHashtagsElement) {
-      evt.preventDefault();
-      closePopupImageModification();
-    }
+  function uploadSuccess() {
+    closePopupImageModification();
+    var successTemplateElement = document.querySelector('#success').content.cloneNode(true);
+    document.querySelector('main').appendChild(successTemplateElement);
   }
+
+  function uploadError() {
+    closePopupImageModification();
+    var errorTemplateElement = document.querySelector('#error').content.cloneNode(true);
+    document.querySelector('main').appendChild(errorTemplateElement);
+    var errorButtonElement = document.querySelector('.error__button');
+    errorButtonElement.addEventListener('click', function () {
+      document.querySelector('section.error').remove();
+      window.constant.uploadFileElement.click();
+    });
+  }
+
+  function init() {
+    window.constant.uploadFileElement.addEventListener('change', openPopupImageModification);
+    window.constant.uploadCancelButtonElement.addEventListener('click', closePopupImageModification);
+    window.constant.uploadCancelButtonElement.addEventListener('keydown', function (evt) {
+      if (evt.key === 'Enter') {
+        closePopupImageModification();
+      }
+    });
+
+    window.constant.uploadImageFormElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+      var formData = new FormData(window.constant.uploadImageFormElement);
+      window.backend.save(formData, uploadSuccess, uploadError);
+    });
+
+    document.addEventListener('click', function (evt) {
+      var successMessageElement = document.querySelector('section.success');
+      var errorMessageElement = document.querySelector('section.error');
+      var successButtonElement = document.querySelector('.success__button');
+      if (evt.target.contains(errorMessageElement)) {
+        errorMessageElement.remove();
+      } else if (evt.target === successButtonElement || evt.target.contains(successMessageElement)) {
+        successMessageElement.remove();
+      }
+    });
+  }
+
+  window.upload = {
+    init: init
+  };
 
 })();
